a00a0a3965beec0b14610f893f9f5f42602c1ca3
Vinyug <vincentguyomarch89@gmail.com>
2023-01-05 18:32:31 +0100

    show cooker : model, twig

--- app/Models/Cooker.php
+++ app/Models/Cooker.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cooker extends Model
 {  
     protected $fillable = [
         'user_id', 'firstname', 'lastname', 'slug', 'img',
     ];
+
+    // récupérer les posts associé à un cooker
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class)->orderBy('id', 'desc');
+    }
 }
