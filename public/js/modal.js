e7b175808feca21fe7012165907ef46c5e5d6211
Vinyug <vincentguyomarch89@gmail.com>
2023-01-10 12:07:56 +0100

    js modal delete user, cooker, post and comment

---
+++ public/js/modal.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// modal delete confirmation
+
+const modalDeletePost = document.getElementById('modal-delete-post');
+const cancelDeletePost = document.getElementById('cancel-delete-post');
+const modalDeleteComment = document.getElementById('modal-delete-comment');
+const cancelDeleteComment = document.getElementById('cancel-delete-comment');
+const modalDeleteCooker = document.getElementById('modal-delete-cooker');
+const cancelDeleteCooker = document.getElementById('cancel-delete-cooker');
+const modalDeleteUser = document.getElementById('modal-delete-user');
+const cancelDeleteUser = document.getElementById('cancel-delete-user');
+
+
+window.onclick = function(e) {
+    if (e.target == modalDeletePost || e.target == cancelDeletePost) {
+        modalDeletePost.style.display = "none";
+    }
+}
+
+window.onclick = function(e) {
+    if (e.target == modalDeleteComment || e.target == cancelDeleteComment) {
+        modalDeleteComment.style.display = "none";
+    }
+}
+
+window.onclick = function(e) {
+    if (e.target == modalDeleteCooker || e.target == cancelDeleteCooker) {
+        modalDeleteCooker.style.display = "none";
+    }
+}
+
+window.onclick = function(e) {
+    if (e.target == modalDeleteUser || e.target == cancelDeleteUser) {
+        modalDeleteUser.style.display = "none";
+    }
+}
